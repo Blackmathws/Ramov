2f8b10d4615a71056ee2952aba4f6e007a35cf4f
Leonardo Coêlho <leocoelho.pi@gmail.com>
2025-06-16 19:41:50 -0300

    refactor: Muda links do whatsapp

--- src/components/utils/WhatsappFixed.tsx
+++ src/components/utils/WhatsappFixed.tsx
@@ -3,7 +3,7 @@ import whatsappIcon from '../../assets/icons/whatsapp_fixed.svg'
 
 function WhatsappIcon() {
   return (
-  <a href="https://wa.me/558695776691" target="_blank">
+  <a href="https://api.whatsapp.com/send/?phone=558695776691&text&type=phone_number&app_absent=0" target="_blank">
     <img className="w-12 xs:w-[4.5rem] fixed bottom-6 right-6 hover:scale-110 transition-transform ease-in-out" src={whatsappIcon} alt="whatsapp" />
   </a>
   );
